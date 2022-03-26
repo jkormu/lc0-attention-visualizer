c839457d5ad6dbb85dc64a0f4b5b7d12228af948
jkormu <jussi.kormu@gmail.com>
2022-03-26 13:16:02 +0200

    grid shape update method

--- global_data.py
+++ global_data.py
@@ -3,16 +3,20 @@ from test_array import activations_array
 
 class GlobalData:
     def __init__(self):
-        self.fen = '2kr3r/ppp2b2/2n4p/4p3/Q2Pq1pP/2P1N3/PP3PP1/R1B1KB1R w KQ - 3 18'#'6n1/1p1k4/3p4/pNp5/P1P4p/7P/1P4KP/r7 w - - 2 121'#'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+        self.fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'#'2kr3r/ppp2b2/2n4p/4p3/Q2Pq1pP/2P1N3/PP3PP1/R1B1KB1R w KQ - 3 18'#'6n1/1p1k4/3p4/pNp5/P1P4p/7P/1P4KP/r7 w - - 2 121'#
         self.board = chess.Board(fen=self.fen)
-        self.focused_square_ind = 14
+        self.focused_square_ind = 0
         self.number_of_heads = 8
 
         self.activations = activations_array
         self.visualization_mode = 'ROW'
         self.layer = None
         self.visualization_mode_is_64x64 = False
+        self.subplot_cols = 0
+        self.subplot_rows = 0
+        self.update_grid_shape()
 
+    def update_grid_shape(self):
         if self.number_of_heads <= 8:
             if self.number_of_heads > 4 and self.number_of_heads in (4, 8):
                 self.subplot_cols = min(self.number_of_heads, 4)
@@ -20,9 +24,7 @@ class GlobalData:
                 self.subplot_cols = self.number_of_heads
         else:
             self.subplot_cols = 8
-
-
-        self.subplot_rows = self.number_of_heads // self.subplot_cols #+ (self.number_of_heads % self.subplot_cols != 0)
+        self.subplot_rows = self.number_of_heads // self.subplot_cols
 
     def update_activation_data(self):
         import numpy as np
@@ -33,8 +35,11 @@ class GlobalData:
         self.visualization_mode_is_64x64 = mode == '64x64'
 
     def set_layer(self, layer):
+        #TODO
         self.layer = layer
         self.activations = layer * activations_array
+        self.number_of_heads = 8 #TODO update number of heads in the layer
+        self.update_grid_shape()
 
     def get_head_data(self, head):
 

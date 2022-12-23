9846b691920467cee72115fca00105c1a9a79f62
jkormu <jussi.kormu@gmail.com>
2022-12-23 14:07:03 +0200

    disable debug mode

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from app import app
 import webbrowser
 from threading import Timer
 
+
+
 def open_browser():
     webbrowser.open_new_tab("http://localhost:8050")
 
@@ -11,4 +13,4 @@ def open_browser():
 if __name__ == '__main__':
     Timer(1, open_browser).start() #open browser with delay so server has time to start up
     #app.run_server(debug=True, threaded=True)#, use_reloader=False, dev_tools_hot_reload=False)
-    app.run_server(debug=True, threaded=True, use_reloader=False, dev_tools_hot_reload=False)
+    app.run_server(debug=False, threaded=True, use_reloader=False, dev_tools_hot_reload=False)
